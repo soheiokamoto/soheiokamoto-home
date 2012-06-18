8b8dcbe458f17ee66343ea44c23f7607e59a3c2a
soheiokamoto <sohei.okamoto@gmail.com>
2012-06-18 00:20:47 -0700

    create initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoheiokamotoHome::Application.config.secret_token = 'd4994fdb77144d9d77d73613493993def47d639e1185448698be577fca13a15d9b67e63a97348d45096ff638800678d6ac11382a88f05371df3d309a9eda4d2f'
