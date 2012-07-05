23b412f0d77f3b42432938ff4359420898995cb7
soheiokamoto <sohei.okamoto@gmail.com>
2012-07-05 13:31:26 -0700

    Add content to home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,12 @@ class ApplicationController < ActionController::Base
   helper_method :navigation_menus
 
   def navigation_menus
-    Page.find_by_title('root').child_pages.where(:visible => true).order(:page_order)
+    #Page.find_by_title('root').child_pages.where(:visible => true).order(:page_order)
+    begin
+      Page.find_by_title('root').child_pages.where(:visible => true).order(:page_order)
+    rescue
+      []
+    end
   end
 
 end
